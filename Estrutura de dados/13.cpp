6776b0bae3b4458a0706b7dfd55444f3a92f7b85
Matheus Luna <matheusluna915@gmail.com>
2026-03-11 10:00:24 -0300

    Add files via upload

---
+++ Estrutura de dados/13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	const int size = 5;
+	int num[size] = {2, 4, 6, 8, 10};
+	
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		sum += num[i];
+	}
+	
+	cout << "Soma dos elementos: " << sum << endl;
+	
+	return 0;
+}
