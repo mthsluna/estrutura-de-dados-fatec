b84f7d317c515795a522219f545dd85df6774be9
Matheus Luna <matheusluna915@gmail.com>
2026-03-18 10:26:55 -0300

    Add files via upload

--- Estrutura de dados/Desafios/18.03.2026-desafios/18.03.2026.cpp
+++ Estrutura de dados/Desafios/18.03.2026-desafios/18.03.2026.cpp
@@ -2,6 +2,27 @@
 #include <list>
 using namespace std;
 
-int main(){
-	list<int> numList;
+int main() {
+	int soma = 0;
+	int mult = 1;
+    list <int> numList;
+    numList.push_back(4);
+    numList.push_back(4);
+    numList.push_back(4);
+    numList.push_back(6);
+    numList.push_back(10);
+    
+    for (auto element : numList) {
+    soma += element;
+    mult *= element;
+    }
+    
+    cout << "-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-" << endl;
+    cout << "A soma da lista eh: " << soma << endl;
+    cout << "A multiplicacao da lista eh: " << mult << endl;
+    cout << "-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-" << endl;
+    
+    cout << endl;
+    
+    return 0;
 }
